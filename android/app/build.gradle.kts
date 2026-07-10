d2f91e67021a40b4c0c26bfccabb2d549b3231c7
TranLam <pk1tay123@gmail.com>
2026-07-10 23:31:42 +0700

    fix: ổn định cấu hình build Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,12 +5,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-tasks.whenTaskAdded {
-    if (name.contains("checkAarMetadata")) {
-        enabled = false
-    }
-}
-
 if (file("google-services.json").exists()) {
     apply(plugin = "com.google.gms.google-services")
 }
@@ -21,6 +15,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -53,4 +48,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
